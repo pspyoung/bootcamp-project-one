8aa6ff7813ce69227b518d756f2f6775563101d4
pspyoung <pspyoung2@gmail.com>
2022-03-03 10:28:26 -0800

    Imported useParams in troops.js

--- src/Components/Cards/CardTypes/Troops.js
+++ src/Components/Cards/CardTypes/Troops.js
@@ -1,10 +1,14 @@
 // Troops.js component
 
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import React from 'react';
 
 function Troops(props) {
+	const [clashData, setClashData] = useState(null);
+	const { key } = useParams();
+
 	return (
 		<div className="troops-list">
 			{/* {props.clashData.map((element) => {
