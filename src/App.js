36e83ac4b889b051df13ea99194282b67463f985
pspyoung <pspyoung2@gmail.com>
2022-03-03 13:41:49 -0800

    Imported Routes, Router, Link, Navigate; starting to scaffold out Routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import { useState, useEffect } from 'react';
 import Header from './Components/Header/Header';
 import Cards from './Components/Cards/Cards';
 import Arenas from './Components/Arenas/Arenas';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
 
 function App() {
-	
-
 	return (
 		<div className="mainContainer">
-			<Header />
-			<Arenas />
-			<Cards />
+			<main>
+				<Routes>
+					<Route path />
+					<Route path />
+				</Routes>
+
+				<Header />
+				<Arenas />
+				<Cards />
+			</main>
 		</div>
 	);
 }
