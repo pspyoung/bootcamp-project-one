49ac7c76d0ec0ba9db1ebfbd55c0d716bae12627
pspyoung <pspyoung2@gmail.com>
2022-03-07 21:28:00 -0800

    CSS stylings, adds Media Query for smaller screens

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -4,15 +4,7 @@ import './Homepage.css';
 function Homepage() {
 	return (
 		<div className="HomepageBody">
-			<h1>Welcome to the Homepage</h1>
-			{/* <p> Click "Go Home" any time to return here</p> */}
-			{/* <p>
-				FanArt by{' '}
-				<a href="https://www.deviantart.com/xofks12/art/Clash-Royale-The-royale-arena-612960629">
-					Vichel(xofks12)
-				</a>
-			</p> */}
-
+			<h3 className="homepage">Welcome to the Homepage</h3>
 		</div>
 	);
 }
